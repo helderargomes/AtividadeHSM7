3b01299d8bb587bbdd54374d53a047bce182e7b1
helderargomes <86751305+helderargomes@users.noreply.github.com>
2022-07-10 14:57:52 -0300

    Add files via upload

---
+++ Atividade1.py
@@ -0,0 +1,9 @@
+valor = float(input("Digite o valor do contrato: "))
+
+iss = valor * 0.04
+icms = valor*0.18
+
+print("Valor dos impostos:")
+print()
+print(f"ISS = R$ {iss}")
+print(f"ICMS = R$ {icms}")
